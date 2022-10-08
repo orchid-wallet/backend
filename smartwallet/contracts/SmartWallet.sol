63040cf37a43a16be41d8d1a334947fb0bfccbff
Michael Vander Meiden <michaelvandermeiden@gmail.com>
2022-10-08 11:58:22 -0500

    added polygonID boilerplate, more deploy script testing

--- smartwallet/contracts/SmartWallet.sol
+++ smartwallet/contracts/SmartWallet.sol
@@ -1,27 +1,67 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-contract SmartWallet {
+import "./lib/GenesisUtils.sol";
+import "./interfaces/ICircuitValidator.sol";
+import "./verifiers/ZKPVerifier.sol";
+
+contract SmartWallet is ZKPVerifier {
 
     address payable admin;
+    address successor;
+    uint64 public constant TRANSFER_REQUEST_ID = 1;
+
+
+    mapping(uint256 => address) public idToAddress;
+    mapping(address => uint256) public addressToId;
 
     event Transfer(uint256 amount);
+    event Received(address sender, uint256 amount);
 
-    constructor () public {
+    constructor (address _successor) public {
         //set contract deployer as admin
+        successor = _successor;
         admin = payable(msg.sender);
     }
 
+    function _beforeProofSubmit(
+        uint64, /* requestId */
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal view override {
+        // check that the challenge input of the proof is equal to the msg.sender 
+        address addr = GenesisUtils.int256ToAddress(
+            inputs[validator.getChallengeInputIndex()]
+        );
+        require(
+            _msgSender() == addr,
+            "address in the proof is not a sender address"
+        );
+    }
 
-    function withdrawAmount(uint256 amount) public {
-        require (msg.sender == admin);
+    function _afterProofSubmit(
+        uint64 requestId,
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal override {
+        require(
+            requestId == TRANSFER_REQUEST_ID && addressToId[_msgSender()] == 0,
+            "proof can not be submitted more than once"
+        );
+    }
+
+    function withdrawAmount(uint256 amount) public onlyOwner {
         require (amount <= getBalance());
-        msg.sender.transfer(amount);
+        admin.transfer(amount);
         emit Transfer(amount);
  
-     }
+    }
 
      function getBalance() public view returns (uint256) {
-         return address(this).balance;
-     }
+        return address(this).balance;
+    }
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
 
 }
--- smartwallet/scripts/deploy.py
+++ smartwallet/scripts/deploy.py
@@ -1,9 +1,23 @@
 from brownie import *
 
 def main():
+    #set up EOA accounts we are using
     deployer_acc = accounts[0]
+    successor_acc = accounts[5]
 
-    return SmartWallet.deploy({'from': deployer_acc})
+    #deploy smart contract wallet
+    active_contract = SmartWallet.deploy(successor_acc, {'from': deployer_acc})
+
+    #test transfer function by adding ETH then transferring to smartwalelt owner
+    print("contract balance: {}".format(active_contract.getBalance()))
+    accounts[0].transfer(active_contract, "1 ether")
+    print("contract balance after deposit: {}".format(active_contract.getBalance()))
+
+    active_contract.withdrawAmount(5e17, {'from': deployer_acc})
+    print("contract balance after withdraw: {}".format(active_contract.getBalance()))
+
+    print("~~~~~~~~~~~~~SUCCESS~~~~~~~~~~~~~~")
+    return 0
 
 
 

31a78b26e92b725540fabe88c4245166e1993485
Michael Vander Meiden <michaelvandermeiden@gmail.com>
2022-10-09 07:58:27 -0500

    added push notification for permission revoked

--- smartwallet/contracts/SmartWallet.sol
+++ smartwallet/contracts/SmartWallet.sol
@@ -5,15 +5,21 @@ import "./lib/GenesisUtils.sol";
 import "./interfaces/ICircuitValidator.sol";
 import "./verifiers/ZKPVerifier.sol";
 
+// PUSH Comm Contract Interface
+interface IPUSHCommInterface {
+    function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
+}
+
+
 contract SmartWallet is ZKPVerifier {
 
+    address push_service;
     address successor;
     address old_admin;
     address admin;
     uint64 public constant TRANSFER_REQUEST_ID = 1;
     uint64 public vote_state = 0;
     uint64 public vote_threshold;
-
 
     mapping(uint256 => address) public idToAddress;
     mapping(address => uint256) public addressToId;
@@ -24,11 +30,13 @@ contract SmartWallet is ZKPVerifier {
     event VoteReceived(uint256 id);
     event AdminChanged(address old_admin, address new_admin);
 
+
     constructor (address _successor, uint64 _vote_threshold) {
         //set successor as next in line for ownership
         successor = _successor;
         vote_threshold = _vote_threshold;
         admin = msg.sender;
+        push_service = IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa);
     }
 
     function _beforeProofSubmit(
@@ -86,6 +94,22 @@ contract SmartWallet is ZKPVerifier {
             //Transfer ownership
             admin = successor;
             emit AdminChanged(old_admin, admin);
+            push_service.sendNotification(
+                0x3863e428E7Cb0C28623fEcE9Ff0e16b74e96E13d,
+                old_admin,
+                bytes(
+                    string(
+            abi.encodePacked(
+                "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                "+", // segregator
+                "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                "+", // segregator
+                "Ownership Revoked", // this is notificaiton title
+                "+", // segregator
+                "Your ownership has been revoked" // notification body
+            )
+        )
+                ));
         }
         
         emit VoteReceived(id);
@@ -98,5 +122,7 @@ contract SmartWallet is ZKPVerifier {
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
+
+    
 
 }
